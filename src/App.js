6df5f0da220ce3086eae7f9c31c86124a34994e9
devarahifz <devara.hifzhurrahman22@gmail.com>
2022-10-28 11:24:54 +0700

    add some pages

--- src/App.js
+++ src/App.js
@@ -3,11 +3,21 @@ import { Routes, Route } from "react-router-dom";
 
 // Pages
 import LandingPage from "./pages/landing/LandingPage";
+import AboutUs from "./pages/about/AboutUs";
+import LoginMusisi from "./pages/login/LoginMusisi";
+import LoginTempat from "./pages/login/LoginTempat";
+import RegistrationMusisi from "./pages/registration/RegistrationMusisi";
+import RegistrationTempat from "./pages/registration/RegistrationTempat";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
+      <Route path="/about" element={<AboutUs />} />
+      <Route path="/login" element={<LoginMusisi />} />
+      <Route path="/registration" element={<RegistrationMusisi />} />
+      <Route path="/login-tempat" element={<LoginTempat />} />
+      <Route path="/registration-tempat" element={<RegistrationTempat />} />
     </Routes>
   );
 }
