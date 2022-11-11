ccbcc482b68c9e38c028a9ff8324af6670a7ee0a
devarahifz <devara.hifzhurrahman22@gmail.com>
2022-11-11 16:23:16 +0700

    add gigs reducer & slicer

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authOwnerReducer from "../features/owner/AuthSlice";
 import authUserReducer from "../features/user/AuthSlice";
+import gigReducer from "../features/gig/GigSlice";
 
 export const store = configureStore({
     reducer: {
         authOwner: authOwnerReducer,
-        authUser: authUserReducer
+        authUser: authUserReducer,
+        gig: gigReducer
     }
 });
---
+++ src/features/gig/GigService.js
@@ -0,0 +1,67 @@
+import axios from 'axios';
+import { API_URL } from '../../configs/api';
+
+// Create new gig
+const createGig = async (data, token, id) => {
+  const response = await axios.post(`${API_URL}/gig`, 
+  {
+    owner_id: id,
+    location: data.location,
+    fee: data.fee,
+    title: data.title,
+    description: data.description
+  }, 
+    {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  });
+
+  return response.data;
+}
+
+// Get all gigs
+const getAllGigs = async () => {
+  const response = await axios.get(`${API_URL}/gig`);
+
+  return response.data;
+}
+
+// Get gig by id
+const getGigById = async (id) => {
+  const response = await axios.get(`${API_URL}/gig/${id}`);
+
+  return response.data;
+}
+
+// Update gig
+const updateGig = async (id, data, token) => {
+  const response = await axios.put(`${API_URL}/gig/${id}`, data, {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  });
+
+  return response.data;
+}
+
+// Delete gig
+const deleteGig = async (id, token) => {
+  const response = await axios.delete(`${API_URL}/gig/${id}`, {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  });
+
+  return response.data;
+}
+
+const GigService = {
+  createGig,
+  getAllGigs,
+  getGigById,
+  updateGig,
+  deleteGig
+}
+
+export default GigService;
